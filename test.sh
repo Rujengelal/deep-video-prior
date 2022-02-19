2d465f684d38f5ca892a3462e0e5408c18746ecf
Rujengelal <34258344+Rujengelal@users.noreply.github.com>
2022-02-19 10:18:45 +0545

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python dvp_video_consistency.py --max_epoch 25 --input demo/consistency/Enhancement/bike-packing-input --processed demo/consistency/Enhancement/bike-packing-processed --task enhancement --format png
-# python dvp_video_consistency.py --max_epoch 50 --input demo/consistency/colorization/goat_input --processed demo/consistency/colorization/goat_processed --task colorization --with_IRT 1 --IRT_initialization 1 --output result/colorization
+# python dvp_video_consistency.py --max_epoch 25 --input demo/consistency/Enhancement/bike-packing-input --processed demo/consistency/Enhancement/bike-packing-processed --task enhancement --format png
+python dvp_video_consistency.py --max_epoch 5 --input demo/consistency/colorization/goat_input --processed demo/consistency/colorization/goat_processed --task colorization --with_IRT 1 --IRT_initialization 1 --output result/colorization
 # python dvp_video_consistency.py --max_epoch 25 --input demo/consistency/colorization/goat_input --processed demo/consistency/colorization/goat_processed --task colorization  --output result/colorization
 
 # python dvp_video_consistency.py --max_epoch 25 --input demo/consistency/Dehazing/Haze2_input --processed demo/consistency/consistency/Dehazing/Haze2_processed/ --task dehazing --output result/dehazing
